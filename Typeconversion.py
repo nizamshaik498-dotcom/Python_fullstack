ef83f689de5b814813aef25da3ebc54fb0d02c0c
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-06-09 09:45:19 +0530

    Donne type conversion

---
+++ Typeconversion.py
@@ -0,0 +1,16 @@
+x=float(7)
+print(x)
+
+y=int(9.9)
+print(y)
+
+age=20
+msg="Age is:  "+ str(age)
+
+result= 5+2.0
+print(result)
+print(type(result))
+
+age=int(input("Enter tour age: "))
+next_year=age+1
+print(f"next year you will be {next_year}")
